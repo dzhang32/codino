5cffa8bbfb826d16d7717094d766ca331f486c19
David Zhang <dyzhang32@gmail.com>
2021-09-24 21:51:08 +0100

    add more checking for input to FreqTable

--- codino/FreqTable.py
+++ codino/FreqTable.py
@@ -1,8 +1,6 @@
 class FreqTable:
 
-    # placeholder _freq
     def __init__(self, freq):
-
         self._freq = freq
 
     @property
@@ -11,14 +9,9 @@ class FreqTable:
 
     @freq.setter
     def freq(self, value):
-        poss_keys = self.freq.keys()
-
-        if any([k not in poss_keys for k in value.keys()]):
-            raise KeyError("Keys must be one of" +
-                           ", ".join(self.freq.keys()))
-
-        elif any([v > 1 or v < 0 for v in value.values()]):
-            raise ValueError("Values must be between 0 and 1")
+        self._check_dict(value)
+        self._check_keys(value)
+        self._check_values(value)
 
         self._freq.update(value)
 
@@ -35,5 +28,25 @@ class FreqTable:
     def get_non_0_freq(self):
         return {k: v for (k, v) in self.freq.items() if v != 0.0}
 
+    @staticmethod
+    def _check_dict(value):
+        if type(value) is not dict:
+            raise TypeError("Value must be a dictionary")
+
+    def _check_keys(self, value):
+        poss_keys = self.freq.keys()
+
+        if any([k not in poss_keys for k in value.keys()]):
+            raise KeyError("Keys must be one of" +
+                           ", ".join(self.freq.keys()))
+
+    @staticmethod
+    def _check_values(value):
+        if any([v > 1 or v < 0 for v in value.values()]):
+            raise ValueError("Values must be between 0 and 1")
+        elif sum(value.values()) != 1:
+            raise ValueError("Values must sum to 1")
+
+
 if __name__ == "__main__":
-    pass
+    x = FreqTable()

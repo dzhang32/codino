47af7d16229f84be668ba8fe59b645561d5ed498
David Zhang <dyzhang32@gmail.com>
2021-09-24 14:59:32 +0100

    finish first iteration of cd to aa

--- codino/Converter.py
+++ codino/Converter.py
@@ -1,14 +1,16 @@
-from codino import CodonDesign, CodonTable
+from codino import CodonDesign, CodonTable, AminoAcidTable
 
 class Converter():
 
     def __init__(self):
         self.cd = CodonDesign()
         self.ct = CodonTable()
+        self.aat = AminoAcidTable()
 
     def to_aa(self, first = {}, second = {}, third = {}):
         self.cd.set_codon_design(first, second, third)
         self._cd_to_ct()
+        self._ct_to_aa()
 
     def _cd_to_ct(self):
         for k1 in self.cd.first:
@@ -23,10 +25,12 @@ class Converter():
                     codon = k1 + k2 + k3
                     freq = x.cd.first[k1] * x.cd.second[k2] * x.cd.third[k3]
 
-        self.ct.set_freq(codon, freq)
+                    self.ct.set_freq(codon, freq)
 
-    def _cc_to_aa(self):
-        pass
+    def _ct_to_aa(self):
+        for aa, codons in self.aat.aa_to_codon.items():
+            freq = sum([self.ct.freq[c] for c in codons])
+            self.aat.set_freq(aa, freq)
 
 if __name__ == "__main__":
 
@@ -34,6 +38,24 @@ if __name__ == "__main__":
     x.to_aa(first = {"A": 1.0},
             second = {"T": 1.0},
             third = {"G": 1.0})
-    print(x.ct.freq)
+    print(x.aat.get_non_0_freq())
+
+    x = Converter()
+    x.to_aa(first={"G": 1.0},
+            second={"A": 1.0},
+            third={"C": 0.41,
+                   "G": 0.59})
+    print(x.aat.get_non_0_freq())
+
+    x = Converter()
+    x.to_aa(first={"G": 0.77, "A": 0.23},
+            second={"A": 0.51,
+                    "T": 0.26,
+                    "C": 0.23},
+            third={"T": 0.15,
+                   "C": 0.38,
+                   "G": 0.47})
+    print(x.aat.get_non_0_freq())
+
 
 
--- codino/__init__.py
+++ codino/__init__.py
@@ -1,3 +1,4 @@
 from .CodonDesign import CodonDesign
 from .CodonTable import CodonTable
+from .AminoAcidTable import AminoAcidTable
 from .Converter import Converter

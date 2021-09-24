b08a910317fd4c17fdf225dc7d1a82cb1cc720a9
David Zhang <dyzhang32@gmail.com>
2021-09-24 11:01:20 +0100

    change name to codino

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="codon_aa_freq",
+    name="codino",
     version="0.0.1",
     author="David Zhang",
     author_email="dyzhang32@gmail.com",

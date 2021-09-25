c23c60a6950cdb70ece06ccf96fd169a44bcca42
David Zhang <dyzhang32@gmail.com>
2021-09-25 09:11:52 +0100

    add tests for FreqTable

---
+++ test/test_FreqTable.py
@@ -0,0 +1,29 @@
+import pytest
+
+from codino import FreqTable
+
+ft = FreqTable({"A": 0, "T": 0, "C": 0, "G": 0})
+
+def test_get_freq_property():
+    assert ft.freq == {"A": 0, "T": 0, "C": 0, "G": 0}
+    assert ft.freq == ft._freq
+
+def test_set_freq_property():
+    ft.freq = {"A": 0.5, "C": 0.5}
+    assert ft.freq == {"A": 0.5, "T": 0, "C": 0.5, "G": 0}
+
+def test_get_non_0_freq_property():
+    assert ft.get_non_0_freq() == {"A": 0.5, "C": 0.5}
+
+def test_set_freq_catches_input_errors():
+    with pytest.raises(TypeError):
+        ft.freq = 5
+
+    with pytest.raises(KeyError):
+        ft.freq = {"B": 0.0}
+
+    with pytest.raises(ValueError):
+        ft.freq = {"A": 5.0}
+
+    with pytest.raises(ValueError):
+        ft.freq = {"A": 0.8, "T": 0.5}
